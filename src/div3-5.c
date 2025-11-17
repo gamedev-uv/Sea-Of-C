9601a08206ec7164bae49ec654e1cc5d4d2127db
GameDev_UV <99119317+gamedev-uv@users.noreply.github.com>
2025-11-17 18:02:29 +0530

    Fixed Div 3,5 Program

--- src/div3-5.c
+++ src/div3-5.c
@@ -9,15 +9,17 @@ int main()
     scanf("%d", &num);
 
     printf("\n=== [OUTPUT] ===");
+    if(num % 3 != 0 && num % 5 != 0)
+    {
+        printf("\nDivisible by neither 3 nor 5");
+        return 0;
+    }
+    
     if(num % 3 == 0)
         printf("\nThe number is divisible by 3");
-
+    
     if(num % 5 == 0)
-    {
         printf("\nThe number is divisible by 5");
-        return 1;
-    }
 
-    printf("\nDivisible by neither 3 nor 5");
     return 0;
 }
